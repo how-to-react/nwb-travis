dd9da70d95ce30762105d701449cc313484786cb
Stephen Fraser <stephen@blackgrid.co.uk>
2017-05-02 20:44:14 +0100

    Added newer module as dependency to prove NPM module works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-export default 'Welcome to nwb-travis';
+import { mapToArray } from '@how-to-react/nwb-semantic-release';
+
+const message = mapToArray({'one': 'Welcome to nwb-travis'})[0];
+
+export default message;
